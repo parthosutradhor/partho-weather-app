f9c314d614fbc7865b29d4da7aff9daa0af0035d
parthosutradhor <parthosutradhor@gmail.com>
2020-12-24 11:14:59 +0600

    adding condition icon

--- public/js/main.js
+++ public/js/main.js
@@ -3,6 +3,7 @@ const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const content1 = document.querySelector('#message-1')
 const content2 = document.querySelector('#message-2')
+const content3 = document.getElementById('conditionIcon')
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -10,14 +11,17 @@ weatherForm.addEventListener('submit', (e) => {
 
     content1.textContent = 'Loading...'
     content2.textContent = ''
+    content3.src = ''
     
     fetch('/weather?search=' + location).then((response) => {
     response.json().then((data) => {
         if(data.error){
             content1.textContent = data.error
+            content3.src = ''
         }else{
             content1.textContent = data.location
             content2.textContent = data.forecastMsg
+            content3.src = data.conditionIcon
         }
     })
 })
